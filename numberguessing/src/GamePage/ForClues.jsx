9471171da05d3f6a81f023662808de7b31888b3d
Tharunkumar001 <tharung1411@gmail.com>
2021-04-09 10:28:32 +0530

    adding functionality

---
+++ numberguessing/src/GamePage/ForClues.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import {useContext} from 'react';
+
+
+
+const Clue = (props) => {
+    const [clues,setClues] = useState("");
+
+    // function oddEven() {
+
+    // }
+
+    // function lessMore() {
+
+    // }
+
+    // function SpecialNumber() {
+
+    // }
+   
+
+    return(
+        <div>
+            <h3>tharunkumar</h3>
+        </div>
+    )
+}
+
+export default Clue;
