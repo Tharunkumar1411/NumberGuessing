4a3489aeac430e7f9782f13123bf63e3254b2209
Tharunkumar001 <tharung1411@gmail.com>
2021-04-10 19:05:52 +0530

    ad

--- numberguessing/src/GamePage/ForClues.jsx
+++ numberguessing/src/GamePage/ForClues.jsx
@@ -1,32 +1,63 @@
 import React, { useState } from 'react';
-import {useContext} from 'react';
+import {useContext,useEffect} from 'react';
 
 
 
 const Clue = (props) => {
-    const sysNum = props.number;
+    
     const [clues,setClues] = useState({clueOne:"",clueTwo:"",clueThree:"",clueFour:"",clueFive:""});
 
-    function oddEven() {
-        if(sysNum%2 === 0){
-            var even = "even";
-            setClues({clueOne:even})
-        }else{
-            setClues({clueOne:"odd"})
+    useEffect(() =>{
+        const sysNum = props.number;
+        const guessNum = props.guess;
+
+        function higherLesser() {
+            if(sysNum > guessNum){
+                var less = "lesser";
+                setClues({clueOne:less});
+            }else{
+                var high = "higher";
+                setClues({clueOne:high});
+            }
+            console.log(clues.clueOne);
+
         }
-    }
+        higherLesser();
 
-    function lessMore() {
-        var MoreTen = sysNum + 10;
-        var lessTen = sysNum - 10;
+    
+        function oddEven() {
+            if(sysNum%2 === 0){
+                var even = "even";
 
-        setClues({clueTwo:`between ${lessTen} and ${MoreTen}`});
-    }
+                
+                setClues({clueTwo:even})
+            }else{
+                var odd = "odd";
 
-    // function SpecialNumber() {
+                setClues({clueTwo:odd})
+            }
+            console.log(clues.clueTwo);
+        }
+        oddEven();
+        
+        // function lessMore() {
+        //     var MoreTen = sysNum + 10;
+        //     var lessTen = sysNum - 10;
+    
+        //     setClues({clueThree:`between ${lessTen} and ${MoreTen}`});
+        //     console.log(clues.clueTwo);
+        // }
+    
+        // higherLesser();
 
-    // }
-   
+    
+        // function SpecialNumber() {
+    
+        // }
+       
+    },[])
+    
+    
 
     return(
         <div>
